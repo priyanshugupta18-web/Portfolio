d5f1be86685704718e01b8e917e6dc0beab6da33
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-06-08 16:25:10 +0530

    going to start the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { NavLink, Link, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element="./Components/Hero/Hero.jsx" />
+      </Routes>
     </>
   )
 }
