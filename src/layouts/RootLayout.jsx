dcf5277094ecad70e8bdb231a4da2f216a140fec
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-08-17 00:49:28 +0530

    added meta tags

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,7 +3,7 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { Outlet } from 'react-router-dom'
 import { Footer, PageLoader, ScrollProgress } from '../components'
 
-const loaderDuration = 1850
+const loaderDuration = 2400
 
 export default function RootLayout() {
   const [loading, setLoading] = useState(true)
